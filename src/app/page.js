6fbb569b88fc49c6c08c00b9746050b0882e7243
BoatGT-9 <twz192545@gmail.com>
2023-10-24 15:59:01 +0700

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,59 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
+// import * from 'react';
+import React,{useState,useEffect} from 'react';
+import { initializeConnector } from '@web3-react/core';
+import { MetaMask } from '@web3-react/metamask';
+import AppBar from '@mui/material/AppBar';
+import Navbar from './components/Navbar';
 
+import { ethers } from 'ethers';
+import { formatEther,parseUnits } from '@ethersproject/units';
+import abi from './abi.json'
+
+
+const [metamask,hooks] = initializeConnector((actions) => new MetaMask({ actions }))
+const{useChainId, useAccounts, useIsActivating,useIsActive,useProvider} = hooks 
+const contractChain = 11155111
+const contractAddress = "0x67F6D236A48a0e500cbaA588E20ffADe6E17e77a"
 export default function Home() {
+
+  const chainId = useChainId()
+  const accounts = useAccounts()
+  const IsActive = useIsActive()
+
+  const provider = useProvider()
+  const [error, setError] = useState(undefined)
+	
+  useEffect(() => {
+    void metamask.connectEagerly().catch(() => {
+      console.debug('Failed to connect eagerly to metamask')
+    })
+  }, [])
+
+  const handleConnect = () => {
+    metamask.activate(contractChain)
+  }
+
+  const handleDisconnect = () => {
+    metamask.resetState()
+  }
+  
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div>
+      <Navbar IsActive={IsActive} onClick={handleConnect} onDisconnect={handleDisconnect} accounts={accounts}/>
+
+      <div className='card '>
+      <p>chainId: { contractChain }</p>
+      <p>isActive: { IsActive.toString() }</p>
+      <p>accounts: { accounts ? accounts[0] : '' }</p>
+      { IsActive ?
+        <input type='button' onClick={handleDisconnect} value={'Disconnect'} />
+        :
+        <input type='button' onClick={handleConnect} value={'Connect'} />
+
+        
+      }
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
