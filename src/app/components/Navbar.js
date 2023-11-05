a1d6ca37189c1f9ec5d358a80eedce4793c71109
BoatGT-9 <twz192545@gmail.com>
2023-11-05 23:26:25 +0700

    newcommit

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -15,7 +15,7 @@ export default function ButtonAppBar({
   
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static"  sx={{backgroundColor:"#cb997e"}}>
         <Toolbar>
           <IconButton
             size="large"
--- src/app/page.js
+++ src/app/page.js
@@ -1,59 +1,85 @@
-"use client"
+"use client";
 // import * from 'react';
-import React,{useState,useEffect} from 'react';
-import { initializeConnector } from '@web3-react/core';
-import { MetaMask } from '@web3-react/metamask';
-import AppBar from '@mui/material/AppBar';
-import Navbar from './components/Navbar';
+import React, { useState, useEffect } from "react";
+import { initializeConnector } from "@web3-react/core";
+import { MetaMask } from "@web3-react/metamask";
+import Navbar from "./components/Navbar";
+import Card from "./components/Card";
 
-import { ethers } from 'ethers';
-import { formatEther,parseUnits } from '@ethersproject/units';
-import abi from './abi.json'
+import { _ethers } from "ethers";
+import { formatEther } from "@ethersproject/units";
 
+import { ethers } from "ethers";
+import { parseUnits } from "@ethersproject/units";
+import abi from "./abi.json";
 
-const [metamask,hooks] = initializeConnector((actions) => new MetaMask({ actions }))
-const{useChainId, useAccounts, useIsActivating,useIsActive,useProvider} = hooks 
-const contractChain = 11155111
-const contractAddress = "0x67F6D236A48a0e500cbaA588E20ffADe6E17e77a"
+const [metamask, hooks] = initializeConnector(
+  (actions) => new MetaMask({ actions })
+);
+const { useChainId, useAccounts, useIsActivating, useIsActive, useProvider } =
+  hooks;
+const contractChain = 11155111;
+const contractAddress = "0x67F6D236A48a0e500cbaA588E20ffADe6E17e77a";
 export default function Home() {
+  const chainId = useChainId();
+  const accounts = useAccounts();
+  const IsActive = useIsActive();
 
-  const chainId = useChainId()
-  const accounts = useAccounts()
-  const IsActive = useIsActive()
+  const provider = useProvider();
+  const [error, setError] = useState(undefined);
 
-  const provider = useProvider()
-  const [error, setError] = useState(undefined)
-	
   useEffect(() => {
     void metamask.connectEagerly().catch(() => {
-      console.debug('Failed to connect eagerly to metamask')
-    })
-  }, [])
+      console.debug("Failed to connect eagerly to metamask");
+    });
+  }, []);
+
+  const [balance, setBalance] = useState("");
+  useEffect(() => {
+    const fetchBalance = async () => {
+      const signer = provider.getSigner();
+      const smartContract = new ethers.Contract(contractAddress, abi, signer);
+      const myBalance = await smartContract.balanceOf(accounts[0])
+      console.log(formatEther(myBalance));
+      setBalance(formatEther(myBalance));
+    };
+    if(IsActive){
+      fetchBalance();
+    }
+  },[IsActive]);
+
 
   const handleConnect = () => {
-    metamask.activate(contractChain)
-  }
+    metamask.activate(contractChain);
+  };
 
   const handleDisconnect = () => {
-    metamask.resetState()
-  }
-  
+    metamask.resetState();
+  };
+
   return (
     <div>
-      <Navbar IsActive={IsActive} onClick={handleConnect} onDisconnect={handleDisconnect} accounts={accounts}/>
-
-      <div className='card '>
-      <p>chainId: { contractChain }</p>
-      <p>isActive: { IsActive.toString() }</p>
-      <p>accounts: { accounts ? accounts[0] : '' }</p>
-      { IsActive ?
-        <input type='button' onClick={handleDisconnect} value={'Disconnect'} />
-        :
-        <input type='button' onClick={handleConnect} value={'Connect'} />
-
+      <Navbar
+        IsActive={IsActive}
+        onClick={handleConnect}
+        onDisconnect={handleDisconnect}
+        accounts={accounts}
+        />
+      {/* {IsActive && <div>balance = {balance}  </div>} */}
+      <Card IsActive={IsActive} accounts={accounts} balance={balance} provider={provider} />
+      {/* <div className="card  ">
+         <div className="card-content margin: 0 auto">
+          <p>chainId: {contractChain}</p>
+          <p>isActive: {IsActive.toString()}</p> 
+          <p>accounts: {accounts ? accounts[0] : ""}</p>
+          {IsActive ? (
+            <input type="button" onClick={handleDisconnect} value={"Disconnect"}/>
+            ) : (
+              <input type="button" onClick={handleConnect} value={"Connect"} />
+              )}
         
-      }
-      </div>
+        </div>
+      </div> */}
     </div>
-  )
+  );
 }
